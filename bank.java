1c257e2472d20c5f7f7c0b0b1d30de1368063150
manjulahumane <manjulab.xworkz@gmail.com>
2021-12-20 17:45:05 +0530

    uploading

---
+++ bank.java
@@ -0,0 +1,15 @@
+class bank{
+public static void main(String[] args){
+
+short[] balance={1111,2222,3333,444,3335,666,777,888,999,110};
+System.out.println(balance[0]);
+System.out.println(balance[1]);
+System.out.println(balance[2]);
+System.out.println(balance[3]);
+System.out.println(balance[4]);
+System.out.println(balance[5]);
+System.out.println(balance[6]);
+System.out.println(balance[8]);
+System.out.println(balance[9]);
+}
+}
